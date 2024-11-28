e19fcc9be1e64b1da8fcd194151f4b6a20336a42
fabianscharfetter69 <117461947+fabianscharfetter69@users.noreply.github.com>
2024-11-28 14:41:38 +0100

    gitignore test

--- VisualObjectRecognition/VisualObjectRecognition.Server/obj/Debug/net8.0/VisualObjectRecognition.Server.AssemblyInfo.cs
+++ VisualObjectRecognition/VisualObjectRecognition.Server/obj/Debug/net8.0/VisualObjectRecognition.Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VisualObjectRecognition.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d4d36a42ba99a730a10939e7284823068073c50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64ad312635229dff4fcb224075a1d4218db0f2bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("VisualObjectRecognition.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VisualObjectRecognition.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
