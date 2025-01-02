4736c0ccc33dbc825a5e417bbbce552e2bcd37e2
Tobias Ziller <tobias.ziller@htl-saalfelden.at>
2025-01-02 16:44:08 +0100

    Reapply "Responsive"
    This reverts commit ef4a98f0bd83749de415ab547437f3a2d4e1fd3f.

--- VisualObjectRecognition/visualobjectrecognition.client/src/Header.tsx
+++ VisualObjectRecognition/visualobjectrecognition.client/src/Header.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import './Stylesheets/Header.css';
+import React, { useState } from 'react';
 
 const Header: React.FC = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => setIsMenuOpen(prev => !prev);
+
     return (
         <header className="header">
             <div className="logo-container">
@@ -10,7 +14,13 @@ const Header: React.FC = () => {
                     <img src="./public/logo.svg" alt="Logo" className="logo" />
                 </Link>
             </div>
-            <nav className="nav-links">
+            <div className="menu-icon" onClick={toggleMenu}>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+
+            <nav className={`nav-links ${isMenuOpen ? 'open' : ''}`}>
                 <a href="https://lagerage.at/home">SelfStorage</a>
                 <a href="https://lagerage.at/home/immobilien">Immobilien</a>
                 <Link to="/about">{"\u00DCber uns"}</Link>
