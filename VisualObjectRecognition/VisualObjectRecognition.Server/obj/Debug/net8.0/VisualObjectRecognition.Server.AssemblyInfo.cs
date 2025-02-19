64c7092ec5f5b64b3d8884dd670013a9614bc505
fabianscharfetter <fabian.scharfetter@htl-saalfelden.at>
2025-02-19 15:31:05 +0100

    Update

--- VisualObjectRecognition/VisualObjectRecognition.Server/obj/Debug/net8.0/VisualObjectRecognition.Server.AssemblyInfo.cs
+++ VisualObjectRecognition/VisualObjectRecognition.Server/obj/Debug/net8.0/VisualObjectRecognition.Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VisualObjectRecognition.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e264b5c247930ff0ee8cdb87d939b2678560a18e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54bda511ca155cbd0ca64dd8b352199fe7470257")]
 [assembly: System.Reflection.AssemblyProductAttribute("VisualObjectRecognition.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VisualObjectRecognition.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
