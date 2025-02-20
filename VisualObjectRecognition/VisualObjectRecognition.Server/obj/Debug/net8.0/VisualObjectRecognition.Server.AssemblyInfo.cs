d52627d3d81d39271aa2956cb699efb4fbed0987
Tobias Ziller <tobias.ziller@htl-saalfelden.at>
2025-02-20 10:48:31 +0100

    changes frontend

--- VisualObjectRecognition/VisualObjectRecognition.Server/obj/Debug/net8.0/VisualObjectRecognition.Server.AssemblyInfo.cs
+++ VisualObjectRecognition/VisualObjectRecognition.Server/obj/Debug/net8.0/VisualObjectRecognition.Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VisualObjectRecognition.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+156462980613e471f2d223b7c594fffa73211fb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab51cfb316b48da3ef175ccb06481119623e3962")]
 [assembly: System.Reflection.AssemblyProductAttribute("VisualObjectRecognition.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VisualObjectRecognition.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
